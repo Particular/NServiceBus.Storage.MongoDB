7b93d92803745633906ad11586621eb982fd55b9
Brandon Ording <bording@gmail.com>
2019-05-16 11:20:12 -0400

    Remove unused Constants

--- src/NServiceBus.Storage.MongoDB/Database/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NServiceBus.Storage.MongoDB
-{
-    static class Constants
-    {
-        public const string SubscriptionCollectionName = "subscriptions";
-        public const string DeduplicationCollectionName = "deduplication";
-        public const string SagaUniqueIdentityCollectionName = "saga_unique_ids";
-    }
-}

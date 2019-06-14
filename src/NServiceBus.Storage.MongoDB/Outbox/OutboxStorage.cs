21e72150c74ddfe02f96cb8d537218ea6a167fcc
Bob Langley <bob.langley@particular.net>
2019-06-14 12:25:15 -0700

    Correct serialization for TransportOperation dictionaries
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Bob Langley <bob.langley@particular.net>
    Co-authored-by: Indu Alagarsamy <indu.alagarsamy@particular.net>

--- src/NServiceBus.Storage.MongoDB/Outbox/OutboxStorage.cs
+++ src/NServiceBus.Storage.MongoDB/Outbox/OutboxStorage.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Options;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 using NServiceBus.Features;
+using NServiceBus.Outbox;
 
 namespace NServiceBus.Storage.MongoDB
 {
@@ -20,6 +24,16 @@ namespace NServiceBus.Storage.MongoDB
         {
             var client = context.Settings.Get<Func<IMongoClient>>(SettingsKeys.MongoClient)();
             var databaseName = context.Settings.Get<string>(SettingsKeys.DatabaseName);
+
+            if (!BsonClassMap.IsClassMapRegistered(typeof(TransportOperation)))
+            {
+                BsonClassMap.RegisterClassMap<TransportOperation>(cm =>
+                {
+                    cm.AutoMap();
+                    cm.MapMember(c => c.Headers).SetSerializer(new DictionaryInterfaceImplementerSerializer<Dictionary<string, string>>(DictionaryRepresentation.ArrayOfDocuments));
+                    cm.MapMember(c => c.Options).SetSerializer(new DictionaryInterfaceImplementerSerializer<Dictionary<string, string>>(DictionaryRepresentation.ArrayOfDocuments));
+                });
+            }
 
             context.Container.ConfigureComponent(() => new OutboxPersister(client, databaseName), DependencyLifecycle.SingleInstance); //TODO
         }

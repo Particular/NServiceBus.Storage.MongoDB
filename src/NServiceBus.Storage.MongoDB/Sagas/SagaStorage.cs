c10d74bb09b3f3eea7f1a37579f31ab73d1531b4
Bob Langley <bob.langley@particular.net>
2019-06-27 16:01:37 -0700

    Create collections/indexes during SagaStorage feature setup

--- src/NServiceBus.Storage.MongoDB/Sagas/SagaStorage.cs
+++ src/NServiceBus.Storage.MongoDB/Sagas/SagaStorage.cs
@@ -1,4 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
 using NServiceBus.Features;
+using NServiceBus.Sagas;
 
 namespace NServiceBus.Storage.MongoDB
 {
@@ -15,6 +22,53 @@ namespace NServiceBus.Storage.MongoDB
             if (!context.Settings.TryGet(SettingsKeys.VersionElementName, out string versionElementName))
             {
                 versionElementName = "_version";
+            }
+
+            var collectionIndexKeys = new Dictionary<string, List<string>>();
+
+            var client = context.Settings.Get<Func<IMongoClient>>(SettingsKeys.MongoClient)();
+            var databaseName = context.Settings.Get<string>(SettingsKeys.DatabaseName);
+
+            var db = client.GetDatabase(databaseName);
+
+            var collectionNames = db.ListCollectionNames().ToList();
+
+            foreach (var name in collectionNames)
+            {
+                collectionIndexKeys.Add(name, new List<string>());
+
+                var indexes = db.GetCollection<BsonDocument>(name).Indexes.List().ToList();
+
+                foreach (var index in indexes)
+                {
+                    collectionIndexKeys[name].AddRange(index["key"].AsBsonDocument.Names);
+                }
+            }
+
+            var collectionNamingConvention = context.Settings.Get<Func<Type, string>>(SettingsKeys.CollectionNamingConvention);
+
+            var sagaMetaDataCollection = context.Settings.Get<SagaMetadataCollection>();
+
+            foreach (var sagaMetaData in sagaMetaDataCollection)
+            {
+                var expectedCollectionName = collectionNamingConvention(sagaMetaData.SagaEntityType);
+
+                var collectionExists = collectionIndexKeys.ContainsKey(expectedCollectionName);
+
+                if (!collectionExists)
+                {
+                    db.CreateCollection(expectedCollectionName);
+                }
+
+                if (sagaMetaData.TryGetCorrelationProperty(out SagaMetadata.CorrelationPropertyMetadata property))
+                {
+                    var propertyElementName = sagaMetaData.SagaEntityType.GetElementName(property.Name);
+                    if (!collectionExists || !collectionIndexKeys[expectedCollectionName].Contains(propertyElementName))
+                    {
+                        var indexModel = new CreateIndexModel<BsonDocument>(new BsonDocumentIndexKeysDefinition<BsonDocument>(new BsonDocument(propertyElementName, 1)), new CreateIndexOptions() { Unique = true });
+                        db.GetCollection<BsonDocument>(expectedCollectionName).Indexes.CreateOne(indexModel);
+                    }
+                }
             }
 
             context.Container.ConfigureComponent(() => new SagaPersister(versionElementName), DependencyLifecycle.SingleInstance);

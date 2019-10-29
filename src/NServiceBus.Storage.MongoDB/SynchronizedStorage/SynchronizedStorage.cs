07974222c7866b1545927c9bed9905f13e303600
Tomasz Masternak <tomasz.masternak@particular.net>
2019-10-29 11:05:59 +0100

    Apply suggestions from code review
    Co-Authored-By: Tim Bussmann <timbussmann@users.noreply.github.com>

--- src/NServiceBus.Storage.MongoDB/SynchronizedStorage/SynchronizedStorage.cs
+++ src/NServiceBus.Storage.MongoDB/SynchronizedStorage/SynchronizedStorage.cs
@@ -37,10 +37,10 @@
 
                         //HINT: cluster configuration check is needed as the built-in checks, executed during "StartTransaction() call,
                         //      do not detect if the cluster configuration is a supported one. Only the version ranges are validated.
-                        //      Without this check the exception would still be thrown but only later during message processing. 
+                        //      Without this check, exceptions will be thrown during message processing. 
                         if (clusterType != ClusterType.ReplicaSet && clusterType != ClusterType.Sharded)
                         {
-                            throw new Exception($"Transactions are supported only on replica set or sharded clusters. Disable support for transactions by calling 'EndpointConfiguration.UsePersistence<{nameof(MongoPersistence)}>().UseTransactions(false)'.");
+                            throw new Exception($"Transactions are only supported on replica sets or sharded clusters. Disable support for transactions by calling 'EndpointConfiguration.UsePersistence<{nameof(MongoPersistence)}>().UseTransactions(false)'.");
                         }
 
                         try

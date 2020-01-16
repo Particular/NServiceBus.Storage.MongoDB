f2f36c24932cbcf6228827fa7a3732630ea5bb0c
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-01-16 14:49:35 -0500

    Add acceptance tests with disabled transactions (#46)
    * add acceptance tests with disabled transactions
    
    * Review comments
    
    * Rename proj to trigger tests
    
    Co-authored-by: Daniel Marbach <daniel.marbach@openplace.net>

---
+++ src/NServiceBus.Storage.MongoDB.NoTx.AcceptanceTests/ConfigureEndpointMongoPersistence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting.Support;
+
+class ConfigureEndpointMongoPersistence : IConfigureEndpointTestExecution
+{
+    const string databaseName = "AcceptanceTests";
+    IMongoClient client;
+
+    public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
+    {
+        var containerConnectionString = Environment.GetEnvironmentVariable("NServiceBusStorageMongoDB_ConnectionString");
+
+        client = string.IsNullOrWhiteSpace(containerConnectionString) ? new MongoClient() : new MongoClient(containerConnectionString);
+
+        configuration.UsePersistence<MongoPersistence>()
+            .MongoClient(client)
+            .DatabaseName(databaseName)
+            .UseTransactions(false); // disable transactions
+
+        return Task.FromResult(0);
+    }
+
+    public async Task Cleanup()
+    {
+        try
+        {
+            await client.DropDatabaseAsync(databaseName);
+        }
+        // ReSharper disable once EmptyGeneralCatchClause
+        catch (Exception)
+        { }
+    }
+}
+

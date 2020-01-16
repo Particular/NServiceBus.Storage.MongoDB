3643f52a9f29ee0fcf01d3e9c5363d4c939e31c6
Tim <bussmann.t@gmail.com>
2020-01-16 17:38:17 +0100

    add acceptance tests with disabled transactions

---
+++ src/NServiceBus.Storage.MongoDB.AcceptanceTests.NoTx/TestSuiteConstraints.cs
@@ -0,0 +1,21 @@
+﻿using NServiceBus.AcceptanceTesting.Support;
+
+namespace NServiceBus.AcceptanceTests
+{
+    public partial class TestSuiteConstraints
+    {
+        public bool SupportsDtc => false;
+
+        public bool SupportsCrossQueueTransactions => true;
+
+        public bool SupportsNativePubSub => false;
+
+        public bool SupportsNativeDeferral => true;
+
+        public bool SupportsOutbox => false;
+
+        public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointAcceptanceTestingTransport(SupportsNativePubSub, SupportsNativeDeferral);
+
+        public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointMongoPersistence();
+    }
+}

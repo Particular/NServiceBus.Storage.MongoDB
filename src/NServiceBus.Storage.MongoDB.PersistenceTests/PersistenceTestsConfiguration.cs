9003c26925e042f799a0ad4caf7512ac05fa3672
lailabougria <laila.bougria@particular.net>
2020-07-17 10:13:23 +0200

    wip

---
+++ src/NServiceBus.Storage.MongoDB.PersistenceTests/PersistenceTestsConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace NServiceBus.PersistenceTesting
+{
+    using System.Threading.Tasks;
+    using NServiceBus.Outbox;
+    using NServiceBus.Sagas;
+    using Persistence;
+    using Timeout.Core;
+    using Unicast.Subscriptions.MessageDrivenSubscriptions;
+
+    public class PersistenceTestsConfiguration : IPersistenceTestsConfiguration
+    {
+        public PersistenceTestsConfiguration()
+        {
+            SagaIdGenerator = new
+        }
+
+        public bool SupportsDtc => false;
+        public bool SupportsOutbox => true;
+        public bool SupportsFinders => true;
+        public bool SupportsSubscriptions => true;
+        public bool SupportsTimeouts => false;
+        public bool SupportsOptimisticConcurrency => false;
+        public bool SupportsPessimisticConcurrency => true;
+
+        public ISagaIdGenerator SagaIdGenerator { get; }
+        public ISagaPersister SagaStorage { get; }
+        public ISynchronizedStorage SynchronizedStorage { get; }
+        public ISynchronizedStorageAdapter SynchronizedStorageAdapter { get; }
+        public ISubscriptionStorage SubscriptionStorage { get; }
+        public IPersistTimeouts TimeoutStorage { get; }
+        public IQueryTimeouts TimeoutQuery { get; }
+        public IOutboxStorage OutboxStorage { get; }
+
+        public Task Configure()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Cleanup()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

12f19aad85271e602e5d573dede153ec20e0b86a
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-03-07 12:08:53 +0100

    ensure persistence assembly is scanned

---
+++ src/NServiceBus.Storage.MongoDB.AcceptanceTests/TestSetup.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus;
+using NUnit.Framework;
+
+[SetUpFixture]
+public class TestSetup
+{
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        // ensure the persistence assembly is loaded into the AppDomain because it needs its features to be scanned to work properly.
+        typeof(MongoPersistence).ToString();
+    }
+}

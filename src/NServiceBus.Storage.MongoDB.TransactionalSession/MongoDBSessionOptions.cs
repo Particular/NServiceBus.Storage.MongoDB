950e779bd4d40308c5d0a53b9fd3eb26c6c256bc
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-09-05 12:25:44 +0200

    implement TransactionalSession support

---
+++ src/NServiceBus.Storage.MongoDB.TransactionalSession/MongoDBSessionOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace NServiceBus.TransactionalSession;
+
+/// <summary>
+/// The options allowing to control the behavior of the transactional session.
+/// </summary>
+public sealed class MongoDBSessionOptions : OpenSessionOptions
+{
+}
---
+++ src/NServiceBus.Storage.MongoDB.TransactionalSession/MongoDBTransactionalSessionExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace NServiceBus.TransactionalSession;
+
+using Features;
+using NServiceBus.Configuration.AdvancedExtensibility;
+
+/// <summary>
+/// MongoDB persistence extensions for <see cref="ITransactionalSession"/> support.
+/// </summary>
+public static class MongoDBTransactionalSessionExtensions
+{
+    /// <summary>
+    /// Enables transactional session for this endpoint.
+    /// </summary>
+    public static PersistenceExtensions<MongoPersistence> EnableTransactionalSession(
+        this PersistenceExtensions<MongoPersistence> persistenceExtensions)
+    {
+        persistenceExtensions.GetSettings().EnableFeatureByDefault(typeof(TransactionalSession));
+
+        return persistenceExtensions;
+    }
+}

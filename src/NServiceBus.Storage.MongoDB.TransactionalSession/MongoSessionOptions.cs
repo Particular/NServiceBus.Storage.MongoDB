515c17c4f7b145d066f4a24a63d05891ca3c9016
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-09-06 08:34:25 +0200

    align namespace declaration

--- src/NServiceBus.Storage.MongoDB.TransactionalSession/MongoSessionOptions.cs
+++ src/NServiceBus.Storage.MongoDB.TransactionalSession/MongoSessionOptions.cs
@@ -1,8 +1,9 @@
-﻿namespace NServiceBus.TransactionalSession;
-
-/// <summary>
-/// The options allowing to control the behavior of the transactional session.
-/// </summary>
-public sealed class MongoSessionOptions : OpenSessionOptions
+﻿namespace NServiceBus.TransactionalSession
 {
+    /// <summary>
+    /// The options allowing to control the behavior of the transactional session.
+    /// </summary>
+    public sealed class MongoSessionOptions : OpenSessionOptions
+    {
+    }
 }

8964aa00cb85334dd18475010c874379490414ec
danielmarbach <daniel.marbach@openplace.net>
2022-09-06 14:29:57 +0200

    One more namespace

--- src/NServiceBus.Storage.MongoDB.TransactionalSession.AcceptanceTests/Infrastructure/TransactionSessionWithOutboxEndpoint.cs
+++ src/NServiceBus.Storage.MongoDB.TransactionalSession.AcceptanceTests/Infrastructure/TransactionSessionWithOutboxEndpoint.cs
@@ -1,19 +1,20 @@
-﻿namespace NServiceBus.TransactionalSession.AcceptanceTests;
-
-using System;
-using System.Threading.Tasks;
-using NServiceBus.AcceptanceTesting.Support;
-
-public class TransactionSessionWithOutboxEndpoint : TransactionSessionDefaultServer
+﻿namespace NServiceBus.TransactionalSession.AcceptanceTests
 {
-    public override Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointConfiguration,
-        Func<EndpointConfiguration, Task> configurationBuilderCustomization) =>
-        base.GetConfiguration(runDescriptor, endpointConfiguration, async configuration =>
-        {
-            configuration.ConfigureTransport().TransportTransactionMode = TransportTransactionMode.ReceiveOnly;
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus.AcceptanceTesting.Support;
 
-            configuration.EnableOutbox();
+    public class TransactionSessionWithOutboxEndpoint : TransactionSessionDefaultServer
+    {
+        public override Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointConfiguration,
+            Func<EndpointConfiguration, Task> configurationBuilderCustomization) =>
+            base.GetConfiguration(runDescriptor, endpointConfiguration, async configuration =>
+            {
+                configuration.ConfigureTransport().TransportTransactionMode = TransportTransactionMode.ReceiveOnly;
 
-            await configurationBuilderCustomization(configuration);
-        });
+                configuration.EnableOutbox();
+
+                await configurationBuilderCustomization(configuration);
+            });
+    }
 }

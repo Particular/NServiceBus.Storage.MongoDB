13f486914950c0c7ad4ed98f36eae21cc14e7c42
danielmarbach <daniel.marbach@openplace.net>
2022-09-07 14:14:36 +0200

    Make the session a first class citizen

--- src/NServiceBus.Storage.MongoDB/SynchronizedStorage/IMongoSessionProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NServiceBus.Storage.MongoDB
-{
-    using global::MongoDB.Driver;
-
-    interface IMongoSessionProvider
-    {
-        IClientSessionHandle MongoSession { get; }
-    }
-}

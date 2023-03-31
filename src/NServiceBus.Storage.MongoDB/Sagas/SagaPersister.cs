d5f2cee7aaa83f5b4a7d08128bac45852d6cd70c
Daniel Marbach <daniel.marbach@nservicebus.com>
2023-03-31 12:02:23 +0200

    Use saga data type to save and update sagas (#488)
    * Switch persister to use explicit type
    
    # Conflicts:
    #	src/NServiceBus.Storage.MongoDB.AcceptanceTests/NServiceBus.Storage.MongoDB.AcceptanceTests.csproj
    #	src/NServiceBus.Storage.MongoDB.NoTx.AcceptanceTests/NServiceBus.Storage.MongoDB.NoTx.AcceptanceTests.csproj
    #	src/NServiceBus.Storage.MongoDB.PersistenceTests/NServiceBus.Storage.MongoDB.PersistenceTests.csproj
    #	src/NServiceBus.Storage.MongoDB.Tests/NServiceBus.Storage.MongoDB.Tests.csproj
    #	src/NServiceBus.Storage.MongoDB/NServiceBus.Storage.MongoDB.csproj

--- src/NServiceBus.Storage.MongoDB/Sagas/SagaPersister.cs
+++ src/NServiceBus.Storage.MongoDB/Sagas/SagaPersister.cs
@@ -21,7 +21,7 @@
             var storageSession = (StorageSession)session;
             var sagaDataType = sagaData.GetType();
 
-            var document = sagaData.ToBsonDocument();
+            var document = sagaData.ToBsonDocument(sagaDataType);
             document.Add(versionElementName, 0);
 
             await storageSession.InsertOneAsync(sagaDataType, document).ConfigureAwait(false);
@@ -33,7 +33,7 @@
             var sagaDataType = sagaData.GetType();
 
             var version = storageSession.RetrieveVersion(sagaDataType);
-            var document = sagaData.ToBsonDocument().SetElement(new BsonElement(versionElementName, version + 1));
+            var document = sagaData.ToBsonDocument(sagaDataType).SetElement(new BsonElement(versionElementName, version + 1));
 
             var result = await storageSession.ReplaceOneAsync(sagaDataType, filterBuilder.Eq(idElementName, sagaData.Id) & filterBuilder.Eq(versionElementName, version), document).ConfigureAwait(false);
 

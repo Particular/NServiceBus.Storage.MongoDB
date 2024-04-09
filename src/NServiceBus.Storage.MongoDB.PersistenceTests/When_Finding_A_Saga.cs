f506ece9c8ceb6664e6af27c048894be9dcb01f2
Adam Wright <awright18@gmail.com>
2024-04-09 10:14:22 -0500

    Adding failing test for custom serializer

---
+++ src/NServiceBus.Storage.MongoDB.PersistenceTests/When_Finding_A_Saga.cs
@@ -0,0 +1,53 @@
+﻿namespace NServiceBus.PersistenceTesting
+{
+    using System;
+    using System.Threading.Tasks;
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization;
+    using MongoDB.Bson.Serialization.Serializers;
+    using NUnit.Framework;
+    using Sagas;
+
+    public class When_finding_a_saga : SagaPersisterTests
+    {
+        [Test]
+        public async Task Should_find_saga_with_custom_id()
+        {
+            BsonSerializer.RegisterSerializationProvider(new GuidSerializerProvider());
+
+            var saga = new When_completing_a_saga_loaded_by_id.TestSagaData { SomeId = Guid.NewGuid().ToString(), DateTimeProperty = DateTime.UtcNow };
+            await SaveSaga(saga);
+
+            var context = configuration.GetContextBagForSagaStorage();
+            using (var completeSession = configuration.CreateStorageSession())
+            {
+                await completeSession.Open(context);
+
+                var sagaData = await configuration.SagaStorage.Get<When_completing_a_saga_loaded_by_id.TestSagaData>(saga.Id, completeSession, context);
+
+                await configuration.SagaStorage.Complete(sagaData, completeSession, context);
+                await completeSession.CompleteAsync();
+            }
+
+            var completedSaga = await GetById<When_completing_a_saga_loaded_by_id.TestSagaData>(saga.Id);
+            Assert.Null(completedSaga);
+        }
+
+        public When_finding_a_saga(TestVariant param) : base(param)
+        {
+        }
+    }
+
+    public class GuidSerializerProvider : IBsonSerializationProvider
+    {
+        public IBsonSerializer GetSerializer(Type type)
+        {
+            if (type == typeof(Guid))
+            {
+                return new GuidSerializer(BsonType.String);
+            }
+
+            return null;
+        }
+    }
+}

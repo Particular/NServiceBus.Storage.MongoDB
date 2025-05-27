1e5306559ec80fd6277b3fe909083e1890196398
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-05-27 16:32:46 +0200

    Extract registration

--- src/NServiceBus.Storage.MongoDB/MongoPersistence.cs
+++ src/NServiceBus.Storage.MongoDB/MongoPersistence.cs
@@ -21,29 +21,7 @@
         {
             Defaults(s =>
             {
-                try
-                {
-                    // By default, we are using the Standard representation for Guids which is the Guid representation
-                    // that should be used moving forward as of version 2.19 of the MongoDB .NET Driver
-                    // https://www.mongodb.com/docs/drivers/csharp/v2.19/fundamentals/serialization/guid-serialization/
-                    BsonSerializer.TryRegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
-                }
-                catch (BsonSerializationException ex)
-                {
-                    // TryRegisterSerializer can throw if a serializer for Guid is already registered,
-                    // but the serializer is a different instance than the one we are trying to register
-                    // In that case we assume that the serializer is already registered correctly. If it is not,
-                    // then the unspecified format with binary representation will be used which leads to runtime
-                    // exceptions when reading/writing Guids
-                    //
-                    // Looking up the serializer automatically caches the serializer, so we can only do this in the catch block
-                    if (BsonSerializer.LookupSerializer<Guid>() is GuidSerializer { GuidRepresentation: GuidRepresentation.Unspecified, Representation: BsonType.Binary })
-                    {
-                        throw new Exception("A GuidSerializer using the Unspecified representation is already registered which" +
-                                            " indicates the default serializer has already been used. Ensure to register the GuidSerializer" +
-                                            " with the preferred representation before using the mongodb client as early as possible.", ex);
-                    }
-                }
+                SafeRegisterDefaultGuidSerializer();
 
                 s.SetDefault(SettingsKeys.MongoClient, static () =>
                 {
@@ -62,6 +40,33 @@
             Supports<StorageType.Sagas>(s => s.EnableFeatureByDefault<SagaStorage>());
             Supports<StorageType.Outbox>(s => s.EnableFeatureByDefault<OutboxStorage>());
             Supports<StorageType.Subscriptions>(s => s.EnableFeatureByDefault<SubscriptionStorage>());
+        }
+
+        internal static void SafeRegisterDefaultGuidSerializer()
+        {
+            try
+            {
+                // By default, we are using the Standard representation for Guids which is the Guid representation
+                // that should be used moving forward as of version 2.19 of the MongoDB .NET Driver
+                // https://www.mongodb.com/docs/drivers/csharp/v2.19/fundamentals/serialization/guid-serialization/
+                BsonSerializer.TryRegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
+            }
+            catch (BsonSerializationException ex)
+            {
+                // TryRegisterSerializer can throw if a serializer for Guid is already registered,
+                // but the serializer is a different instance than the one we are trying to register
+                // In that case we assume that the serializer is already registered correctly. If it is not,
+                // then the unspecified format with binary representation will be used which leads to runtime
+                // exceptions when reading/writing Guids
+                //
+                // Looking up the serializer automatically caches the serializer, so we can only do this in the catch block
+                if (BsonSerializer.LookupSerializer<Guid>() is GuidSerializer { GuidRepresentation: GuidRepresentation.Unspecified, Representation: BsonType.Binary })
+                {
+                    throw new Exception("A GuidSerializer using the Unspecified representation is already registered which" +
+                                        " indicates the default serializer has already been used. Ensure to register the GuidSerializer" +
+                                        " with the preferred representation before using the mongodb client as early as possible.", ex);
+                }
+            }
         }
 
         internal static MongoDatabaseSettings DefaultDatabaseSettings { get; } = new MongoDatabaseSettings

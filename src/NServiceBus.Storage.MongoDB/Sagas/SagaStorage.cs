d0a7c1b85edc6f0be9399fca94a1fde8748b5703
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-08-12 18:04:15 +0200

    Cleanup SagaStorage

--- src/NServiceBus.Storage.MongoDB/Sagas/SagaStorage.cs
+++ src/NServiceBus.Storage.MongoDB/Sagas/SagaStorage.cs
@@ -1,8 +1,6 @@
 ﻿namespace NServiceBus.Storage.MongoDB;
 
-using System;
 using Features;
-using global::MongoDB.Driver;
 using Microsoft.Extensions.DependencyInjection;
 using Sagas;
 
@@ -23,32 +21,6 @@ class SagaStorage : Feature
             versionElementName = SagaPersister.DefaultVersionElementName;
         }
 
-        if (!context.Settings.TryGet<Func<IMongoClient>>(SettingsKeys.MongoClient, out Func<IMongoClient>? client))
-        {
-            return;
-        }
-
-        string? databaseName = context.Settings.Get<string>(SettingsKeys.DatabaseName);
-        Func<Type, string>? collectionNamingConvention =
-            context.Settings.Get<Func<Type, string>>(SettingsKeys.CollectionNamingConvention);
-
-        var collectionSettings = new MongoCollectionSettings
-        {
-            ReadConcern = ReadConcern.Majority,
-            ReadPreference = ReadPreference.Primary,
-            WriteConcern = WriteConcern.WMajority
-        };
-
-        IMongoCollection<OutboxRecord>? outboxCollection = client().GetDatabase(databaseName)
-            .GetCollection<OutboxRecord>(collectionNamingConvention(typeof(OutboxRecord)), collectionSettings);
-        if (outboxCollection == null)
-        {
-            throw new ArgumentNullException(
-                $"Collection {outboxCollection} should be created in the database : {databaseName}");
-        }
-
-        var memberMapCache = new MemberMapCache();
-
-        context.Services.AddSingleton<ISagaPersister>(new SagaPersister(versionElementName, memberMapCache));
+        context.Services.AddSingleton<ISagaPersister>(new SagaPersister(versionElementName, new MemberMapCache()));
     }
 }

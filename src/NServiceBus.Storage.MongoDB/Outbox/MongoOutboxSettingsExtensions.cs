00d6270b3fd79f6ef11fb89c746bf5dc4ad04ed9
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-08-26 19:23:56 +0200

    Make outbox settings extensions still fluent since others are too

--- src/NServiceBus.Storage.MongoDB/Outbox/MongoOutboxSettingsExtensions.cs
+++ src/NServiceBus.Storage.MongoDB/Outbox/MongoOutboxSettingsExtensions.cs
@@ -13,21 +13,25 @@ public static class MongoOutboxSettingsExtensions
     /// <summary>
     /// Configures the amount of time to keep outbox deduplication data.
     /// </summary>
-    public static void TimeToKeepOutboxDeduplicationData(this OutboxSettings outboxSettings, TimeSpan timeToKeepOutboxDeduplicationData)
+    public static OutboxSettings TimeToKeepOutboxDeduplicationData(this OutboxSettings outboxSettings, TimeSpan timeToKeepOutboxDeduplicationData)
     {
         ArgumentNullException.ThrowIfNull(outboxSettings);
 
         outboxSettings.GetSettings().GetOrCreate<OutboxPersistenceConfiguration>().TimeToKeepDeduplicationData = timeToKeepOutboxDeduplicationData;
+
+        return outboxSettings;
     }
 
     /// <summary>
     /// When retrieving outbox messages, the persister tries to load the outbox records assuming the new or the old
     /// outbox record schema. For collections that are known to only contain the new schema this fallback can be disabled.
     /// </summary>
-    public static void DisableReadFallback(this OutboxSettings outboxSettings)
+    public static OutboxSettings DisableReadFallback(this OutboxSettings outboxSettings)
     {
         ArgumentNullException.ThrowIfNull(outboxSettings);
 
         outboxSettings.GetSettings().GetOrCreate<OutboxPersistenceConfiguration>().ReadFallbackEnabled = false;
+
+        return outboxSettings;
     }
 }

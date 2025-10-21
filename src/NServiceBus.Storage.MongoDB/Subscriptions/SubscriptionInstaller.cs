d97a337ef6041ae5c4f60a8189b63ef1a7a9cb29
Andreas Öhlund <andreas.ohlund@particular.net>
2025-10-21 14:21:40 +0200

    Cleanup

--- src/NServiceBus.Storage.MongoDB/Subscriptions/SubscriptionInstaller.cs
+++ src/NServiceBus.Storage.MongoDB/Subscriptions/SubscriptionInstaller.cs
@@ -5,10 +5,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using global::MongoDB.Driver;
 using Installation;
-using Microsoft.Extensions.DependencyInjection;
 using Settings;
 
-sealed class SubscriptionInstaller(IReadOnlySettings settings, IServiceProvider serviceProvider) : INeedToInstallSomething
+sealed class SubscriptionInstaller(IReadOnlySettings settings, IMongoClientProvider clientProvider) : INeedToInstallSomething
 {
     public async Task Install(string identity, CancellationToken cancellationToken = default)
     {
@@ -16,9 +15,6 @@ sealed class SubscriptionInstaller(IReadOnlySettings settings, IServiceProvider 
         var databaseSettings = settings.Get<MongoDatabaseSettings>();
         var collectionSettings = settings.Get<MongoCollectionSettings>();
         var collectionNamingConvention = settings.Get<Func<Type, string>>(SettingsKeys.CollectionNamingConvention);
-
-        // We have to resolve the client provider here because at the time of the creation of the installer the provider might not be registered yet.
-        var clientProvider = serviceProvider.GetRequiredService<IMongoClientProvider>();
 
         await CreateInfrastructureForSubscriptionTypes(clientProvider.Client, databaseSettings, databaseName, collectionSettings, collectionNamingConvention, cancellationToken)
             .ConfigureAwait(false);
